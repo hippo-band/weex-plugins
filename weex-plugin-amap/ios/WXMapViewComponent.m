c7320c239f262b72d39f2043f5cbff1d0079292c
yangshengtao <shengtao.yst@alibaba-inc.com>
2017-01-10 10:22:01 +0800

    add annotations in mapview

--- weex-plugin-amap/ios/WXMapViewComponent.m
+++ weex-plugin-amap/ios/WXMapViewComponent.m
@@ -17,6 +17,7 @@
 @implementation WXMapViewComponent
 {
     CLLocationCoordinate2D _centerCoordinate;
+    NSMutableArray *_annotations;
     CGFloat _zoomLevel;
     BOOL _showScale;
     BOOL _showGeolocation;
@@ -84,6 +85,27 @@
         centerCoordinate.longitude = [attributes[@"center"][0] doubleValue];
         [self.mapView setCenterCoordinate:centerCoordinate];
     }
+    
+    if (attributes[@"points"]) {
+        NSArray *points = attributes[@"points"];
+        if (!_annotations) {
+            _annotations = [NSMutableArray arrayWithCapacity:5];
+        }
+        for (int i = 0; i < points.count; ++i)
+        {
+            NSDictionary *annotations = points[i];
+            NSArray *coordinates = annotations[@"pos"];
+            MAPointAnnotation *a1 = [[MAPointAnnotation alloc] init];
+            CLLocationCoordinate2D coordinate;
+            coordinate.latitude = [coordinates[1] doubleValue];
+            coordinate.longitude = [coordinates[0] doubleValue];
+            a1.coordinate = coordinate;
+            a1.title      = [NSString stringWithFormat:@"%@", annotations[@"title"]];
+            [_annotations addObject:a1];
+            [self.mapView addAnnotations:_annotations];
+            [self.mapView showAnnotations:_annotations edgePadding:UIEdgeInsetsMake(20, 20, 20, 80) animated:YES];
+        }
+    }
 }
 
 - (void)addEvent:(NSString *)eventName
@@ -96,5 +118,34 @@
     
 }
 
+
+#pragma mark - 
+/*!
+ @brief 根据anntation生成对应的View
+ @param mapView 地图View
+ @param annotation 指定的标注
+ @return 生成的标注View
+ */
+- (MAAnnotationView*)mapView:(MAMapView *)mapView viewForAnnotation:(id <MAAnnotation>)annotation {
+    if ([annotation isKindOfClass:[MAPointAnnotation class]])
+    {
+        static NSString *pointReuseIndetifier = @"pointReuseIndetifier";
+        MAPinAnnotationView *annotationView = (MAPinAnnotationView*)[mapView dequeueReusableAnnotationViewWithIdentifier:pointReuseIndetifier];
+        if (annotationView == nil)
+        {
+            annotationView = [[MAPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:pointReuseIndetifier];
+        }
+        
+        annotationView.canShowCallout               = YES;
+        annotationView.animatesDrop                 = YES;
+        annotationView.draggable                    = YES;
+        annotationView.rightCalloutAccessoryView    = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+        annotationView.pinColor                     = [_annotations indexOfObject:annotation] % 3;
+        
+        return annotationView;
+    }
+    
+    return nil;
+}
 
 @end
